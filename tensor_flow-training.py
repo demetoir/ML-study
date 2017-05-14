1af207bb1dfdc857daf6169dd4aede87a2a18d4f
Yujun-desktop <wnsqlehlswk@naver.com>
2017-05-15 01:20:51 +0900

    learning tensorflow stuff
    softmax regression done
    softmax training set added
    
    NN(xor operation) done

--- tensor_flow-training.py
+++ tensor_flow-training.py
@@ -17,32 +17,6 @@ def getShapedList(shape):
     return _getShapedList(shape, 0)
 
 
-def test():
-    shape = shape2D
-    a = tf.placeholder(dtype=tf.float64, shape=shape)
-    b = tf.placeholder(dtype=tf.float64, shape=shape)
-    c = tf.placeholder(dtype=tf.float64, shape=shape)
-
-    x = tf.multiply(a, c)
-    y = tf.add(a, b)
-    z = tf.subtract(b, c)
-
-    sess = tf.Session()
-    input_dict = {a: get1DList(shape),
-                  b: get1DList(shape),
-                  c: get1DList(shape)}
-
-    fetches = [x, y, z]
-    print(sess.run(fetches=x, feed_dict=input_dict))
-    print(sess.run(fetches=y, feed_dict=input_dict))
-    print(sess.run(fetches=z, feed_dict=input_dict))
-
-    print(sess.run(fetches=fetches, feed_dict=input_dict))
-    sess.close()
-
-    pass
-
-
 def ex_constant():
     def test_constant(value, dtype, shape, name):
         print("value =", value, "dtype =", dtype, "shape =", shape, "name =", name)
@@ -55,7 +29,7 @@ def ex_constant():
         [6., tf.float64, [0], "example_1"],
         [6, tf.float16, [1], "example_2"],
         [[1, 2, 3], tf.float64, [3], "example_3"],
-        [get2DList(shape2D), tf.int64, shape2D, "example_4"],
+        [get2DList([3, 4]), tf.int64, [3, 4], "example_4"],
     ]
     print("example of constant")
     for value, dtype, shape, name in exampleList:
@@ -170,7 +144,7 @@ def ex_session():
 # not done yet
 def ex_tensorboard():
     # http: // pythonkim.tistory.com / 38
-
+    # https: // www.youtube.com / watch?v = lmrWZPFYjHM
     summary = tf.merge_all_summaries()
     #
     # with tf.Session() as sess:
@@ -183,10 +157,22 @@ def ex_misc():
     # dtype=tf.float64, shape=[2, 3] returns [[0., 0., 0.],[0., 0., 0.]]
     zeros = tf.zeros(dtype=tf.float64, shape=[2, 3])
     reduce_mean = tf.reduce_mean([[1, 2], [3, 4]])
+    reduce_sum = tf.reduce_sum([[1, 2], [3, 4]])
+    arg_max = tf.arg_max([[1, 2, 3, 4], [8, 7, 6, 5]], 0)
+    arg_min = tf.arg_min([[1, 2, 3, 4], [8, 7, 6, 5]], 1)
+    linspace = tf.linspace(0, 10, 2)
+    range = tf.range(0, 10, 2)
+    random_shuffle = tf.random_shuffle([[1, 2, 3], [5, 6, 7]])
 
     with tf.Session() as sess:
         print(sess.run(zeros))
         print(sess.run(reduce_mean))
+        print(sess.run(reduce_sum))
+        print(sess.run(arg_max))
+        print(sess.run(arg_min))
+        print(sess.run(linspace))
+        print(sess.run(range))
+        print(sess.run(random_shuffle))
 
     return
 
@@ -306,7 +292,8 @@ def training_simple_neural_tensorflowtic():
 
 
 # not yet
-def traing_simple_nearual_network():
+def training_simple_nearual_network():
+    # xor opearation
     x_data = [
         [0, 0],
         [1, 0],
@@ -320,32 +307,139 @@ def traing_simple_nearual_network():
         [0]
     ]
 
-    cost = 1
-    train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+    # input layer
+    X = tf.placeholder(tf.float32, [None, 2], name="X")
+    y = tf.placeholder(tf.float32, [None, 1], name="Y")
 
-    output
-    acc
+    # layer 1
+    layer1len = 10
+    W1 = tf.Variable(tf.random_normal([2, layer1len]), name="W1")
+    B1 = tf.Variable(tf.random_normal([layer1len]), name="B1")
+    layer1 = tf.sigmoid(tf.matmul(X, W1) + B1, name="layer1")
 
-    data = {X: x_data, Y: y_data}
+    # layer 2
+    layer2len = 20
+    W2 = tf.Variable(tf.random_normal([layer1len, layer2len]), name="W2")
+    B2 = tf.Variable(tf.random_normal([layer2len]), name="B2")
+    layer2 = tf.sigmoid(tf.matmul(layer1, W2) + B2, name="layer2")
+
+    # layer 3
+    layer3len = 30
+    W3 = tf.Variable(tf.random_normal([layer2len, layer3len]), name="W3")
+    B3 = tf.Variable(tf.random_normal([layer3len]), name="B3")
+    layer3 = tf.sigmoid(tf.matmul(layer2, W3) + B3, name="layer3")
+
+    # output layer
+    outputLayerLen = 1
+    W4 = tf.Variable(tf.random_normal([layer3len, outputLayerLen]), name="W4")
+    B4 = tf.Variable(tf.random_normal([outputLayerLen]), name="B4")
+    h = tf.sigmoid(tf.matmul(layer3, W4) + B4, name="h")
+
+    # cost function
+    cost = -tf.reduce_mean(y * tf.log(h) + (1 - y) * tf.log(1 - h), name="cost")
+
+    # train operation
+    learning_rate = 0.01
+    train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+    output = tf.cast(h > 0.5, tf.float32, name="output")
+
+    acc = tf.reduce_mean(tf.cast(tf.equal(y, output), dtype=tf.float32), name="acc")
+
+    data = {X: x_data, y: y_data}
     init_op = tf.initialize_all_variables()
     with tf.Session() as sess:
         with tf.device("/gpu:0"):
             sess.run(init_op)
 
             for i in range(10001):
-                sess.run(train, feed_dict=data)
+                sess.run(train_op, feed_dict=data)
 
-                if i % 100:
-                    print(i, sess.run(acc, data), sess.run(cost, data))
+                if i % 100 == 0:
+                    print(i, "acc =", sess.run(acc, data), "cost =", sess.run(cost, data))
+                    # print(sess.run(h, data))
+                    # print(sess.run(y, data))
+                    # print(sess.run(output, data))
+                    # print()
 
             print(sess.run(acc, data))
             print(sess.run(cost, data))
-            print(sess.run(W, data))
+            print(sess.run(output, data))
+            print(sess.run(y, data))
 
     return
 
 
-def train_softmax_regression():
+def training_softmax_regression():
+    # http: // pythonkim.tistory.com / 21
+
+    print("train softmax regression")
+
+    # #x0 x1 x2 y[A   B   C]
+    # 1   2   1   0   0   1     # C
+    # 1   3   2   0   0   1
+    # 1   3   4   0   0   1
+    # 1   5   5   0   1   0     # B
+    # 1   7   5   0   1   0
+    # 1   2   5   0   1   0
+    # 1   6   6   1   0   0     # A
+    # 1   7   7   1   0   0
+    # 출처: http: // pythonkim.tistory.com / 21[파이쿵]
+    fileName = "softmax_regression_train_set.txt"
+    xy = np.loadtxt(fileName, unpack=True, dtype='float32')
+
+    # xy는 6x8. xy[:3]은 3x8. 행렬 곱셈을 하기 위해 미리 transpose.
+    x_data = np.transpose(xy[:3])
+    y_data = np.transpose(xy[3:])
+
+    print('x_data :', x_data.shape)  # x_data : (8, 3)
+    print('y_data :', y_data.shape)  # y_data : (8, 3)
+
+    X = tf.placeholder(tf.float32, [None, 3], name="X")
+    Y = tf.placeholder(tf.float32, [None, 3], name="Y")
+
+    # W = tf.Variable(tf.random_uniform([3, 3]), name="W")
+    W = tf.Variable(tf.zeros([3, 3]))
+
+    # softmax function
+    h = tf.nn.softmax(tf.matmul(X, W), name="h")
+
+    # cost function
+    cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(h), reduction_indices=1), name="cost")
+    # cost = tf.reduce_mean(tf.reduce_sum(Y - h, reduction_indices=1))
+
+    learning_rate = 0.01
+    train = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost, name="train")
+
+    # final predict output
+    output = tf.arg_max(h, 1, name="output")
+
+    # acc = tf.reduce_mean(tf.cast(tf.equal(output, y), dtype=tf.float32), name="acc")
+    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(Y, 1), output), dtype=tf.float32), name="acc")
+
+    init_op = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        with tf.device("/gpu:0"):
+
+            sess.run(init_op)
+            data = {X: x_data, Y: y_data}
+
+            # train step
+            for i in range(5000):
+                sess.run(train, feed_dict=data)
+                if i % 100 == 0:
+                    print(i, "acc =", sess.run(acc, data))
+                    # print(sess.run(cost, data))
+                    # print(sess.run(W, data))
+                    # print(sess.run(tf.argmax(Y, 1), data))
+                    # print(sess.run(output, data))
+                    print()
+
+            # 한번에 여러 개 판단 가능
+            data = {X: [[1, 11, 7], [1, 3, 4], [1, 1, 0]]}
+            print(sess.run(output, data))
+            # d :  ...  [0 1 2]
+
     return
 
 
@@ -355,7 +449,14 @@ if __name__ == '__main__':
     # ex_placeholder()
     # ex_Variable()
     # ex_session()
+    # ex_misc()
+
     # traning_simple_neural_onebyone()
-    training_simple_neural_tensorflowtic()
+
+    # training_simple_neural_tensorflowtic()
+
+    # training_softmax_regression()
+
+    training_simple_nearual_network()
 
     pass
