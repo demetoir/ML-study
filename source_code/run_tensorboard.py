83779618bf69dcd4660040d8911f01ddcf953391
demetoir <wnsqlehlswk@naver.com>
2017-05-23 11:36:34 +0900

    update run_tensorboard.py 자동으로 페이지 열리도록 만듦

--- source_code/run_tensorboard.py
+++ source_code/run_tensorboard.py
@@ -1,17 +1,22 @@
 import os
+import sys
+
 path = os.path
 import subprocess
+import webbrowser
 
 DEFAULT_LOGDIR = ""
 
 # tensorboard --logdir=C:\Users\yujun\Documents\ML-image-classify\tensorboard_log
 dir_log = os.path.join("C:\\Users\\yujun\\Documents\\ML-image-classify", "tensorboard_log")
 cmd = "tensorboard --logdir=" + dir_log
+url = "http://127.0.0.1:6006/"
 
 if __name__ == '__main__':
     print("tensorboard start")
-    p_tensorboard = subprocess.Popen(cmd)
-    print("http://127.0.0.1:6006/")
+    p_tensorboard = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    webbrowser.open(url)
 
     while True:
         print("exit y/n?")
